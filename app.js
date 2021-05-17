60ff19cc738695cf84c3ebd1f2b321d6e65d8c5f
Savo Debeljak <savo.debeljak96@gmail.com>
2021-05-17 20:22:57 +0200

    reorder app.js. add more logging info

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Express = require('express');
-const BodyParser = require('body-parser');
-const Mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
 const winston = require('winston');
@@ -11,9 +11,9 @@ const fs = require('fs');
 
 const routes = require('./routes');
 
-let application = Express();
-application.use(BodyParser.json());
-application.use(BodyParser.urlencoded({extended: true}));
+let application = express();
+application.use(bodyParser.json());
+application.use(bodyParser.urlencoded({extended: true}));
 
 argv = yargs(hideBin(process.argv)).options({
     'p': {
@@ -38,14 +38,26 @@ argv = yargs(hideBin(process.argv)).options({
 }).version('1.0').usage('Usage: $0 -p [int] -v [bool] -c [string]').argv;
 
 const configPath = argv['config-path'];
-const port = argv.port;
+let port = argv.port;
 const verbose = argv.verbose;
+
+let config = null;
+try {
+    config = yaml.load(fs.readFileSync(configPath, 'utf8'));
+} catch (err) {
+    console.log(err.stack || String(err));
+    throw(err);
+}
+
+if (port <= 0) {
+    port = config['server']['port'];
+}
 
 transports = [];
 if (verbose) {
     transports.push(new winston.transports.Console());
 }
-transports.push(new winston.transports.File({'filename': 'logs/requests_log.json'}));
+transports.push(new winston.transports.File({'filename': `${config['log']['location']}/requests_log.json`}));
 
 application.use(expressWinston.logger({
     transports,
@@ -53,12 +65,12 @@ application.use(expressWinston.logger({
         winston.format.timestamp(),
         winston.format.json()
     ),
-    meta: false,
+    meta: true,
     msg: "HTTP  ",
     expressFormat: true,
     colorize: false,
     ignoreRoute: function (req, res) {
-        return false;
+        return req.originalUrl.includes('favicon.ico');
     },
     requestWhitelist: [
         'url',
@@ -67,39 +79,54 @@ application.use(expressWinston.logger({
         'originalUrl',
         'query',
         'body'
-    ]
+    ],
+    dynamicMeta: (req, res) => {
+        const httpRequest = {};
+        const meta = {};
+        if (req) {
+            meta.httpRequest = httpRequest;
+            httpRequest.requestMethod = req.method;
+            httpRequest.requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+            httpRequest.protocol = `HTTP/${req.httpVersion}`;
+            httpRequest.remoteIp = req.ip.indexOf(':') >= 0 ? req.ip.substring(req.ip.lastIndexOf(':') + 1) : req.ip; // just ipv4
+            httpRequest.requestSize = req.socket.bytesRead;
+            httpRequest.userAgent = req.get('User-Agent');
+            httpRequest.referrer = req.get('Referrer');
+        }
+        if (res) {
+            meta.httpRequest = httpRequest;
+            httpRequest.status = res.statusCode;
+            httpRequest.latency = {
+                seconds: Math.floor(res.responseTime / 1000),
+                nanos: (res.responseTime % 1000) * 1000000
+            };
+            if (res.body) {
+                if (typeof res.body === 'object') {
+                    httpRequest.responseSize = JSON.stringify(res.body).length;
+                } else if (typeof res.body === 'string') {
+                    httpRequest.responseSize = res.body.length;
+                }
+            }
+        }
+        return meta;
+    }
 }));
-
 application.use('/api', routes);
 
-serve = (configPath, port, verbose) => {
-    let config = null;
-    try {
-        config = yaml.load(fs.readFileSync(configPath, 'utf8'));
-    } catch (err) {
-        console.log(err.stack || String(err));
-        throw(err);
+application.listen(port, config['server']['host'], () => {
+    let mongoConf = config['mongo'];
+    let connectionURL = '';
+    let usPw = '';
+    if (mongoConf['user'] || mongoConf['pass']) {
+        usPw = `${mongoConf['user']}:${mongoConf['pass']}@`;
     }
-    if (port <= 0) {
-        port = config['server']['port'];
-    }
-    application.listen(port, config['server']['host'], () => {
-        let mongoConf = config['mongo'];
-        let connectionURL = '';
-        let usPw = '';
-        if (mongoConf['user'] || mongoConf['pass']) {
-            usPw = `${mongoConf['user']}:${mongoConf['pass']}@`;
+    connectionURL = `mongodb://${usPw}${mongoConf['host']}:${mongoConf['port']}/${mongoConf['db']}`;
+
+    mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+        if (error) {
+            throw error;
         }
-        connectionURL = `mongodb://${usPw}${mongoConf['host']}:${mongoConf['port']}/${mongoConf['db']}`;
-
-        Mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
-            if (error) {
-                throw error;
-            }
-            console.log(`Connection to ${mongoConf['db']} successful!`);
-        });
+        console.log(`Connection to ${mongoConf['db']} successful!`);
     });
-};
-
-serve(configPath, port, verbose);
+});
 
